4347b9ed1e726e370f1dcf714aa68fb69428d8ab
justinqllvr <justin.quillevere@outlook.fr>
2022-02-10 23:58:46 +0100

    let's start !

--- front-end/pages/api/hello.js
+++ front-end/pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const fetchProjects = async () => {
+  const res = await fetch('http://localhost:1337/api/projets')
+  const reponse = await res.json()
+  return reponse.data
 }
+
+export default fetchProjects;
