c2ee9abfccdffa91c41aa6fc02e96bb668fdadee
justinqllvr <justin.quillevere@outlook.fr>
2022-03-28 18:48:35 +0200

    back to back

--- back-end/src/api/projet/controllers/projet.js
+++ back-end/src/api/projet/controllers/projet.js
@@ -3,9 +3,7 @@
 /**
  *  projet controller
  */
- const collectionType = 'projet'
 
- const schema = require(`../content-types/${collectionType}/schema.json`);
- const createPopulatedController = require("../../../helpers/populate");
- 
- module.exports = createPopulatedController(`api::${collectionType}.${collectionType}`, schema);
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::projet.projet');
