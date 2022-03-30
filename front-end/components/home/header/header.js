0bebeaef65a30614c7af416850c43957d4285222
justinqllvr <justin.quillevere@outlook.fr>
2022-03-30 23:10:15 +0200

    home début anim

--- front-end/components/home/header/header.js
+++ front-end/components/home/header/header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Nav from "../../nav/nav";
 import Image from "next/image";
 import styles from "./header.module.css";
@@ -6,32 +6,34 @@ import scrollDown from "../../../public/assets/svg/scroll_down.svg";
 import Layout from "../../utils/layout";
 
 export default function Header() {
+
   return (
     <Layout>
-<header className={styles.header}>
-      <Nav />
-      <div className={styles.align}>
-        <span className={`p`}>Welcome to my portfolio</span>
-        <span className={`d1`}>Claire Rieusset </span>
-      </div>
-      <div className={`d1 ${styles.graphic}`}>
-        <span>Graphic Designer</span>
-      </div>
-      <div className={styles.bottom}>
-        <div className={`p ${styles.parag}`}>
-          I’am a french design student in Graphic design specialized in UX/UI
-          design.
+      <header className={styles.header}>
+        <Nav />
+        <div className={styles.align}>
+          <span className={`p ${styles.welcomeText}`}>
+            Welcome to my portfolio
+          </span>
+          <span className={`d1 ${styles.name}`}>Claire Rieusset </span>
         </div>
-        <Image
-          src={scrollDown}
-          width={150}
-          height={150}
-          layout="intrinsic"
-          alt="scroll down"
-        ></Image>
-      </div>
-    </header>
+        <div className={`d1 ${styles.graphic}`}>
+          <span>Graphic Designer</span>
+        </div>
+        <div className={styles.bottom}>
+          <div className={`p ${styles.parag}`}>
+            I’am a french design student in Graphic design specialized in UX/UI
+            design.
+          </div>
+          <Image
+            src={scrollDown}
+            width={150}
+            height={150}
+            layout="intrinsic"
+            alt="scroll down"
+          ></Image>
+        </div>
+      </header>
     </Layout>
-    
   );
 }
